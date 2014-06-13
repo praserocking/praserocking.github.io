303348f05b4206fbdef289b874bb30c93021a772
Prasanna <praserocking@gmail.com>
2014-06-13 17:26:02 +0530

    Day two

--- js/index.js
+++ js/index.js
@@ -8,7 +8,24 @@ $(document).ready(function(){
 	$("#vert-menu").sticky({
 		topSpacing:200
 	});
-  	$('#b').click(function() {
-		$("html, body").animate({ scrollTop:$("#bl").offset().top-100},"slow");
+  	$('#skills').click(function() {
+		$("html, body").animate({ 
+			scrollTop:$("#skills-content").offset().top-100
+		},"slow");
+	});
+	$('#projs').click(function() {
+		$("html, body").animate({ 
+			scrollTop:$("#projs-content").offset().top-100
+		},"slow");
+	});
+	$('#exps').click(function() {
+		$("html, body").animate({ 
+			scrollTop:$("#exps-content").offset().top-100
+		},"slow");
+	});
+	$('#cont').click(function() {
+		$("html, body").animate({
+			scrollTop:$("#cont-content").offset().top-100
+		},"slow");
 	});
 });
